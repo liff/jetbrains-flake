900c5ae78b2715f3cd2066b4eb98afb860cdde38
Olli Helenius <liff@iki.fi>
2021-04-03 17:00:27 +0300

    Build updater as a flake

---
+++ updater/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Package version updater for JetBrains flake";
+
+  inputs = {
+    sbt-derivation.url = "github:liff/sbt-derivation/use-findutils";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, sbt-derivation, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        sbt11 = final: prev: { sbt = prev.sbt.override { jre = final.jdk11; }; };
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            sbt11
+            sbt-derivation.overlay
+          ];
+        };
+      in rec {
+
+        packages = flake-utils.lib.flattenTree {
+          jetbrains-updater = pkgs.sbt.mkDerivation {
+            pname = "jetbrains-updater";
+            version = "1";
+
+            depsSha256 = "sha256-YKsTz2lYEv7vV8bzn9mHy37PXPQruvyVbUXsQYyAA1s=";
+
+            src = ./.;
+
+            NATIVE_IMAGE_INSTALLED = "true";
+            GRAALVM_HOME = pkgs.graalvm11-ce;
+
+            buildPhase = ''
+              runHook preBuild
+              sbt nativeImage
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/bin
+              cp target/native-image/jetbrains-updater $out/bin
+              runHook postInstall
+            '';
+          };
+        };
+
+        defaultPackage = packages.jetbrains-updater;
+
+        apps.jetbrains-updater = flake-utils.lib.mkApp { drv = packages.jetbrains-updater; };
+
+        defaultApp = apps.jetbrains-updater;
+      }
+    );
+}
