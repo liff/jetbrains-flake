7668fcfe0d697cfd2537f3bbbaea078c496fe993
Olli Helenius <liff@iki.fi>
2023-06-26 14:45:53 +0300

    Lock nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "JetBrains IntelliJ IDEA";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   outputs = { self, nixpkgs }:
     let systems = [ "x86_64-linux" "aarch64-linux" ];
         libNames = [
